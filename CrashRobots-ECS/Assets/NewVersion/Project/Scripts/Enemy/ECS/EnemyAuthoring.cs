5871238d848ce832a2e78823fc1ef11f02239776
齊藤元紀 <saitogenki20031104@gmail.com>
2024-05-03 20:54:27 +0900

    [Add] ECSで敵がプレイヤーに向けて追尾してくる機能の追加
    敵自体の向きもプレイヤー向きになるよう指定

---
+++ CrashRobots-ECS/Assets/NewVersion/Project/Scripts/Enemy/ECS/EnemyAuthoring.cs
@@ -0,0 +1,35 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemyAuthoring : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public float attackDamage;
+    public float attackRange;
+
+    public float3 lastFramePosition;
+}
+
+public class EnemyBake : Baker<EnemyAuthoring>
+{
+    public override void Bake(EnemyAuthoring authoring)
+    {
+        var paramsData = new ParamsData
+        {
+            MoveSpeed = authoring.moveSpeed,
+            AttackDamage = authoring.attackDamage,
+            AttackRange = authoring.attackRange,
+        };
+        AddComponent(GetEntity(TransformUsageFlags.None), paramsData);
+    }
+}
+
+public struct ParamsData : IComponentData
+{
+    public float MoveSpeed;
+
+    public float AttackDamage;
+    public float AttackRange;   
+}
