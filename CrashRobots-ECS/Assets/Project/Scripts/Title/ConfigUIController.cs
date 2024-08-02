8393bebe7d9eda17591caec4d6ebd481975c5d11
齊藤元紀 <saitogenki20031104@gmail.com>
2024-08-02 19:11:48 +0900

    [Update] 設定項目プログラム更新

--- CrashRobots-ECS/Assets/Project/Scripts/Title/ConfigUIController.cs
+++ CrashRobots-ECS/Assets/Project/Scripts/Title/ConfigUIController.cs
@@ -1,4 +1,3 @@
-using Mono.Cecil.Cil;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,7 +18,9 @@ public class ConfigUIController : MonoBehaviour
     private bool isCanvasActive = false;
     private bool isUseEffects = false;
     private bool isDisplayEntitiesCount = true;
+    private bool isDisplayFPS = true;
     private bool isSpawnLoop = true;
+    private bool isPlayerHP = true;
 
     private const int SpawnCountMax = 15000;
 
@@ -28,7 +29,9 @@ public class ConfigUIController : MonoBehaviour
     public static int SpawnCount { get; private set; }
     public static bool IsUseEffects { get; private set; }
     public static bool IsDisplayEntitiesCount { get; private set; }
+    public static bool IsDisplayFPS { get; private set; }
     public static bool IsSpawnLoop { get; private set; }
+    public static bool IsPlayerHP { get; private set; }
 
     private void Start()
     {
@@ -61,7 +64,9 @@ public class ConfigUIController : MonoBehaviour
         SpawnCount = (int)spawnCount;
         IsUseEffects = isUseEffects;
         IsDisplayEntitiesCount = isDisplayEntitiesCount;
+        IsDisplayFPS = isDisplayFPS;
         IsSpawnLoop = isSpawnLoop;
+        IsPlayerHP = isPlayerHP;
 
         OnBackButton();
     }
@@ -88,8 +93,18 @@ public class ConfigUIController : MonoBehaviour
         isDisplayEntitiesCount = !isDisplayEntitiesCount;
     }
 
+    public void OnClickDisplayFPSToggle()
+    {
+        isDisplayFPS = !isDisplayFPS;
+    }
+
     public void OnClickSpawnLoopToggle()
     {
         isSpawnLoop = !isSpawnLoop; 
+    }
+    
+    public void OnClickPlayerHPToggle()
+    {
+        isPlayerHP = !isPlayerHP;
     }
 }
